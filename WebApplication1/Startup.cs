23664b3c97f02c2f843339e4f337533890834d64
youremywhore <95357313+youremywhore@users.noreply.github.com>
2022-09-28 10:50:52 +0300

    1.3/1.4/1.5

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using WebApplication1.Extensions;
 
 namespace WebApplication1
 {
@@ -26,6 +28,8 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -42,9 +46,16 @@ namespace WebApplication1
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
             }
+            app.UseHsts();
 
             app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+            
             app.UseRouting();
 
             app.UseAuthorization();
